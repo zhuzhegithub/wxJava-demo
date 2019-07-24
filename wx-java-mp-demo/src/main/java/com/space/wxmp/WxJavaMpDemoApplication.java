6231fde997ea587d09d62be7dfb22aebe03b0bce
zhuzhe <zhe.zhu1@outlook.com>
2019-07-24 11:44:51 +0800

    微信公众号Java后端Demo，支持多公众号

---
+++ wx-java-mp-demo/src/main/java/com/space/wxmp/WxJavaMpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.space.wxmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxJavaMpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxJavaMpDemoApplication.class, args);
+    }
+
+}
